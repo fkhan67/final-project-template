57dbeeefd23a3f3fa28c26ad00f49cba8a548ef1
fkhan67 <157556820+fkhan67@users.noreply.github.com>
2024-04-20 01:29:05 -0400

    Update Controller.php

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -4,15 +4,17 @@ namespace app\core;
 
 class Controller
 {
-
-    public function view($path, $includeBundle = false)
+    public function view($path, $data = [], $includeBundle = false)
     {
-        if($includeBundle) {
-            echo vite('main.js');
+        // Extract data variables for use in the included view file
+        extract($data);
+
+        // Include the JavaScript bundle if needed
+        if ($includeBundle) {
+            echo vite('main.js'); // Assuming vite is a function to include JavaScript bundles
         }
+
+        // Include the HTML view file
         include $path;
-
     }
-
-
 }
